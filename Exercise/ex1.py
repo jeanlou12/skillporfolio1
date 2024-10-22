6bdffdf6818e18d664bd78a2688d9c2417cacf64
Jean lou <jeanlou@GitHub.com>
2024-10-22 19:47:56 +0400

    competed

--- Exercise/ex1.py
+++ Exercise/ex1.py
@@ -1,1 +1,7 @@
-print("jean")
+#using string concatenation to combine the variable and print the phrase
+word1 ="Coding " 
+world = "is"
+world = "cool"
+
+#printing the value
+print("Cooding is cool")
--- Exercise/ex2.py
+++ Exercise/ex2.py
@@ -1,1 +1,5 @@
-print("jean")
+# using to varibale and adding them to get the sum
+numbers = [8+ 10]
+total = sum (numbers)
+print (" the sum is:" , total)
+#printing to get the value of the number
