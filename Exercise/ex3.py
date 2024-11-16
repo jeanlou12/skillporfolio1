0532824047a07d33bdc641d051a4667ed2f16433
jeanlou12 <jeanloujeanloudubai@gmail.com>
2024-11-16 19:41:17 +0400

    Add files via upload

---
+++ Exercise/ex3.py
@@ -0,0 +1,26 @@
+# Create a Dictionary to use information
+user_info ={}
+
+# Get the user input for the name ,hometown, and age
+user_info['name'] = input("What is your name? ")
+user_info['hometown'] =input("Where are you hometown? ")
+    
+
+# Loop until a valid age is provided
+while True:
+    age = input("how old are you ")
+    if age.isdigit(): # Check if the input is a number
+        user_info['age'] = int(age)  # Conver into integer
+        break # loop if conversiion is successful
+    else:
+        print("Enter a valid number for your age.")
+
+# Print the value stored in the Dictionary
+print("\nUser Information:")
+print("Name: " + user_info['name'])
+print("Hometown: " + user_info['hometown'])
+print("Age: " + str(user_info['age']))
+
+
+
+
