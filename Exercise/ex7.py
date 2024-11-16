9afefacb38c7bf60c1b4058a34b98db457bf6446
jeanlou12 <jeanloujeanloudubai@gmail.com>
2024-11-16 23:43:54 +0400

    Create ex7.py

---
+++ Exercise/ex7.py
@@ -0,0 +1,15 @@
+for num in range(0,25,1):
+    print(num)
+
+for num in range(40,-2,-2):
+    print(num)
+
+for num in range(20, 41 , 1):
+    print(num)
+
+for num in range(50, 6 , -3):
+    print(num)
+
+
+for num in range(100, 220, 10):
+    print(num)
